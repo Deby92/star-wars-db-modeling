d7622cd4df279233c0d3ea5c82be96e4ed573ac0
Deby92 <d.cantuarias.urrutia@gmail.com>
2022-07-26 04:24:23 +0000

    lunes

--- src/models.py
+++ src/models.py
@@ -24,46 +24,46 @@ class People(Base):
     name = Column(String(250), nullable=False, unique=True)
     height = Column(Integer, nullable=False)
     mass = Column(Integer, nullable=False)
-    hair_color = Column(String(250))
-    skin_color = Column(String(250))
-    eye_color = Column(String(250))
-    gender= Column(String(250))
+    hair_color = Column(String(250),)
+    skin_color = Column(String(250),)
+    eye_color = Column(String(250),)
+    gender= Column(String(250),)
 
 class Planets(Base):
     __tablename__ = 'Planets'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    rotation_period = Column(String(250))
-    orbital_period = Column(String(250))
+    rotation_period = Column(String(250),)
+    orbital_period = Column(String(250),)
     diameter = Column(Integer)
-    climate = Column(String(250))
-    gravity = Column(String(250))
+    climate = Column(String(250),)
+    gravity = Column(String(250),)
 
 class Vehicles(Base):
     __tablename__ = 'Vehicles'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    model = Column(String(250))
-    manufacturer = Column(String(250))
+    model = Column(String(250),)
+    manufacturer = Column(String(250),)
     cost_in_credits = Column(Integer)
-    length = Column(String(250))
+    length = Column(String(250),)
     crew = Column(Integer, nullable=False)
     passengers = Column(Integer, unique=True, nullable=False)
 
 class Favorites_people(Base):
      __tablename__ = 'Favorites_people'
-     user_id = Column(Integer, ForeignKey('user_id'), primary_key=True)
-     people_id = Column(Integer, ForeignKey('People.people_id'))
+     user_id = Column(String(100), ForeignKey('User.id'), primary_key=True)
+     people_id = Column(Integer, ForeignKey('People.people_id'),)
 
 class favorites_vehicles(Base):
      __tablename__ = 'Favorites_vehicles'
-     user_id = Column(Integer, ForeignKey('user_id'), primary_key=True )
-     vehicles_id = Column(Integer, ForeignKey('Vehicles.vehicles_id'))
+     user_id = Column(String(100), ForeignKey('User.id'), primary_key=True )
+     vehicles_id = Column(Integer, ForeignKey('Vehicles.vehicles_id'),)
 
 class favorites_planets(Base):
      __tablename__ = 'Favorites_planets'
-     user_id = Column(Integer, ForeignKey('user_id'), primary_key=True)
-     planets_id = Column(Integer, ForeignKey('Planets.planets_id'))
+     user_id = Column(String(100), ForeignKey('User.id'), primary_key=True)
+     planets_id = Column(Integer, ForeignKey('Planets.planets_id'),)
      
 
 def to_dict(self):
